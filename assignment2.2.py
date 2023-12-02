b0d0c3a0fb600321b1d311d78efa64f344276aeb
zohrehsabzeh <152652486+zohrehsabzeh@users.noreply.github.com>
2023-12-03 00:44:34 +0330

    Create assignment2.2.py

---
+++ assignment2.2.py
@@ -0,0 +1,10 @@
++a=int(input("Enter a: "))
++b=int(input("Enter b: "))
++c=int(input("Enter c: "))
++
++if a+b>c and a+c>b and b+c>a:
++   print("it draw a triangle")
++
++else:
++   print("do not draw a triangle")
+       
