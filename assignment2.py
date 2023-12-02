bbc6eb74059e9f396eaf00225f5b1abf4e806d8c
zohrehsabzeh <152652486+zohrehsabzeh@users.noreply.github.com>
2023-12-03 00:10:08 +0330

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,15 @@
++op= input("Enter op (+,-,*,/): ")
++a=int(input("Enter a: "))
++b=int(input("Enter b: "))
++
++if op=="+":
++    print(a+b)
++
++if op=="-":
++    print(a-b)
++
++if op=="*":
++   print(a*b)
++
++if op=="/":
++    print(a/b)
